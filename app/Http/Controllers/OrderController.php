4dffe327885053bf8d10c778326b922bfe4a8fa5
lhiguitab <lhiguitab@eafit.edu.co>
2025-11-09 02:05:48 -0500

    refactor(Controllers): correction of controllers and views without LANG

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\OrderedProduct;
+use App\Models\Product;
+use App\Services\CartService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function success(Request $request): View|RedirectResponse
+    {
+        $cartProducts = session()->get('cart', []);
+        $products = Product::whereIn('id', array_keys($cartProducts))->get();
+
+        $cartService = new CartService($cartProducts, $products->all());
+
+        if (! $cartService->checkStock()) {
+            return redirect()->back()->with('error', __('One or more products in your cart are out of stock.'));
+        }
+
+        $orderData = [
+            'status' => 'confirmed',
+            'subtotal' => $cartService->calculateSubtotal(),
+            'tax' => $cartService->calculateTax(),
+            'shipping' => CartService::$SHIPPING_COST,
+            'total' => $cartService->calculateTotal(),
+            'payment_id' => uniqid('pay_', true),
+            'user_id' => $request->user()->getId(),
+        ];
+        $order = Order::create($orderData);
+
+        foreach ($products as $product) {
+            $orderedProductData = [
+                'amount' => $cartProducts[$product->getId()],
+                'price' => $product->getPrice(),
+                'order_id' => $order->getId(),
+                'product_id' => $product->getId(),
+            ];
+            OrderedProduct::create($orderedProductData);
+            $product->setStock($product->getStock() - $cartProducts[$product->getId()]);
+            $product->save();
+        }
+
+        session()->forget('cart');
+
+        $viewData = [];
+        $viewData['order'] = $order;
+
+        return view('order.success')->with('viewData', $viewData);
+    }
+}
